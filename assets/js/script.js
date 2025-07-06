7872293ff2ea246d9bf931c0ff019884f64fa2c8
Adrishikhar Chowdhury <amiadrishikhar@gmail.com>
2025-07-06 18:38:57 +0530

    Added Keydown Enter Event

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,39 +9,6 @@ let menu=document.querySelector("#weather-menu");
 const BASE_URL1=`https://api.openweathermap.org/`
 const BASE_URL2=`https://openweathermap.org/img/wn/`;
 
-submit.addEventListener("click",async (e) => {
-    try {
-        weatherInfo.style.backgroundImage = "";
-        weatherInfo.style.backgroundPosition = "";
-        weatherInfo.style.backgroundRepeat = "";
-        weatherInfo.style.backgroundSize = "";
-        weatherInfo.style.height = "";
-        if(!city.value)
-        {
-            return;
-        }
-        e.preventDefault();
-        const WEATHER_URL=`${BASE_URL1}data/2.5/weather?q=${city.value}&appid=${API_KEY}`;
-        const FORECAST_URL=`${BASE_URL1}data/2.5/forecast?q=${city.value}&appid=${API_KEY}`;
-        let response1=await fetch(WEATHER_URL);
-        let data1=await response1.json();
-        let response2=await fetch(FORECAST_URL);
-        let data2=await response2.json();
-        currentWeather(data1);
-        forecastWeather(data2);
-        
-    } catch (error) {
-        currentInfo.innerHTML=""
-        forecastInfo.innerHTML=""
-        weatherInfo.style.backgroundImage=`url(./assets/images/20064239_6199763.svg)`;
-        weatherInfo.style.backgroundPosition= "center";
-        weatherInfo.style.backgroundRepeat= "no-repeat";
-        weatherInfo.style.backgroundSize= "cover";
-        weatherInfo.style.height="20rem";
-        weatherInfo.style.width="100%";
-    }
-})
-
 const currentWeather=(data1)=>{
     weatherInfo.style.display="flex";
     currentInfo.innerHTML="";
@@ -88,6 +55,50 @@ const forecastWeather=(data2)=>{
 const tempConvert=(number)=>{
     return Math.round((parseFloat(`${number}`)-273.15).toFixed(2))
 }
+
+const weatherReport=async (e) => {
+    try {
+        weatherInfo.style.backgroundImage = "";
+        weatherInfo.style.backgroundPosition = "";
+        weatherInfo.style.backgroundRepeat = "";
+        weatherInfo.style.backgroundSize = "";
+        weatherInfo.style.height = "";
+        if(!city.value)
+        {
+            return;
+        }
+        e.preventDefault();
+        const WEATHER_URL=`${BASE_URL1}data/2.5/weather?q=${city.value}&appid=${API_KEY}`;
+        const FORECAST_URL=`${BASE_URL1}data/2.5/forecast?q=${city.value}&appid=${API_KEY}`;
+        let response1=await fetch(WEATHER_URL);
+        let data1=await response1.json();
+        let response2=await fetch(FORECAST_URL);
+        let data2=await response2.json();
+        currentWeather(data1);
+        forecastWeather(data2);
+        
+    } catch (error) {
+        currentInfo.innerHTML=""
+        forecastInfo.innerHTML=""
+        weatherInfo.style.backgroundImage=`url(./assets/images/20064239_6199763.svg)`;
+        weatherInfo.style.backgroundPosition= "center";
+        weatherInfo.style.backgroundRepeat= "no-repeat";
+        weatherInfo.style.backgroundSize= "cover";
+        weatherInfo.style.height="20rem";
+        weatherInfo.style.width="100%";
+    }
+}
+
+submit.addEventListener("click",weatherReport);
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        weatherReport(event);
+    }
+    else{
+        return ;
+    }
+});
 
 if (window.location.hostname === "adrishikharchowdhury.github.io") {
     const baseTag = document.createElement("base");
